4bf8c720c7b35724148c36097e928e0f957a1f3e
farhan ibne saif <farhanibne860@gmail.com>
2022-08-05 04:06:45 -0400

    Add files via upload

---
+++ precache-manifest.1df46ed138807b74ac686e1ab3227e59.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba5a6dfc818bdeaacac7582abfc20641",
+    "url": "/index.html"
+  },
+  {
+    "revision": "212e7bf99ce40483bdd1",
+    "url": "/static/css/main.a1d481cb.chunk.css"
+  },
+  {
+    "revision": "d2c7108d28066e879e57",
+    "url": "/static/js/2.7301bb0c.chunk.js"
+  },
+  {
+    "revision": "6a3fd5f48a97c7577e9c3ede94b4a5b5",
+    "url": "/static/js/2.7301bb0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "212e7bf99ce40483bdd1",
+    "url": "/static/js/main.d2cc22c4.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.d2cc22c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15d4255249dbceabeb89",
+    "url": "/static/js/runtime-main.43480fcd.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
